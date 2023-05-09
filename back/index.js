9de8c33d9c14975aa59d76f7d0fc167113dd1348
pascal-rithi-ke <pascal.rithi@gmail.com>
2023-05-09 23:52:27 +0200

    Amélioration gestion langue + suppresion d'une pokemon

--- back/index.js
+++ back/index.js
@@ -5,18 +5,30 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
-let pokedex = JSON.parse(fs.readFileSync('../data/pokedex.json', 'utf8'));
+const pokedexPath = '../data/pokedex.json';
+let pokedex = JSON.parse(fs.readFileSync(pokedexPath, 'utf8'));
 
 // Retourn la liste des pokemons et leurs infos
 app.get('/', function (req, res) {
     res.send(pokedex)
 });
 
-app.get('/pokemon/:id', function (req, res) {
+// Suppression d'un pokemon
+app.delete('/pokemon/:id', function (req, res) {
     const id = req.params.id;
-    const pokemon = pokedex.find(p => p.id == id);
-    res.send(pokemon);
+    const index = pokedex.findIndex(pokemon => pokemon.id == id);
+    if (index !== -1) {
+        pokedex.splice(index, 1);
+        fs.writeFileSync(pokedexPath, JSON.stringify(pokedex), 'utf8');
+        res.send(pokedex);
+    } else {
+        res.status(404).send('Pokemon not found');
+    }
 });
+
+// ajout d'un pokemon
+
+// modification d'un pokemon
 
 app.listen(3000, function () {
     console.log('server listening on http://localhost:3000/');
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,7 +5,7 @@ import './style/pokeType.css';
 
 function App() {
   const [pokedexData, setPokedexData] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchPokemon, setsearchPokemon] = useState('');
 
   const [isEnglish, setIsEnglish] = useState(true);
   const handleSwitchClick = () => {
@@ -21,34 +21,44 @@ function App() {
   }, []);
 
   const filteredData = pokedexData.filter(pokemon =>
-    (isEnglish ? pokemon.name.english.toLowerCase().includes(searchTerm.toLowerCase()) : false) || pokemon.name.french.toLowerCase().includes(searchTerm.toLowerCase())
+    (isEnglish ? pokemon.name.english.toLowerCase().includes(searchPokemon.toLowerCase()) : false) || pokemon.name.french.toLowerCase().includes(searchPokemon.toLowerCase())
   );
-  
+
+  const handleDeleteClick = (id,name) => {
+    const confirmDelete  = confirm( isEnglish ? `Are you sure you want to delete "${name.english}" ?` : `Voulez-vous vraiment supprimer "${name.french}" ?`);
+    if(confirmDelete){
+      axios.delete(`http://localhost:3000/pokemon/${id}`)
+        .then(res => {
+          setPokedexData(res.data);
+        })
+        .catch(err => console.log(err));
+      }else{
+        return;
+      }
+  };
+
   return (
-    <div>
+    <>
       <div className='headerPage'>
         <h1>Pokedex</h1>
         <input
           type="text"
           placeholder="🇬🇧  & 🇫🇷 🔎"
-          value={searchTerm}
-          onChange={e => setSearchTerm(e.target.value)}
+          value={searchPokemon}
+          onChange={e => setsearchPokemon(e.target.value)}
         />
         <button onClick={handleSwitchClick}>
           {isEnglish ? '🇬🇧 ➡ 🇫🇷' : '🇫🇷 ➡ 🇬🇧'}
         </button>
       </div>
+
       <div className="card-container">
         {filteredData.map(pokemon => (
           <div className="card" key={pokemon.id}>
-            {/*
-            <div className='cardContainer-img'>
-              <img className="card-img-top" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt={`Image of ${pokemon.name.english}`} />
-            </div>
-            */}
+            <button onClick={() => handleDeleteClick(pokemon.id, pokemon.name)}>X</button>
             <div className="card-body">
-              <h5 className="card-title">N°{pokemon.id} - {isEnglish ? pokemon.name.english : pokemon.name.french}</h5>
-              <h5>🇯🇵 {pokemon.name.japanese} | 🇨🇳 : {pokemon.name.chinese}</h5>
+              <h2 className="card-title">N°{pokemon.id} - {isEnglish ? `🇬🇧 ${pokemon.name.english}` : `🇫🇷 ${pokemon.name.french}`}</h2>
+              <h5>🇯🇵 {pokemon.name.japanese} | 🇨🇳 {pokemon.name.chinese} | {isEnglish ? `🇫🇷 ${pokemon.name.french}` : `🇬🇧 ${pokemon.name.english}`}</h5>
               <li className="card-subtitle">Type:
                 {pokemon.type.map(type => (
                   <span className={`typeShape ${type.toLowerCase()}`} key={type}> {type} </span>
@@ -66,7 +76,8 @@ function App() {
           </div>
         ))}
       </div>
-    </div>
+      
+    </>
   );
 }
 export default App;
