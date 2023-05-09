242abc4c3617ffac921fd1e24e666872b15994fb
pascal-rithi-ke <pascal.rithi@gmail.com>
2023-05-09 20:51:54 +0200

    filtre + cardLayout pokemon + changement langue

--- back/index.js
+++ back/index.js
@@ -1,8 +1,21 @@
+import fs from 'fs';
 import express from 'express';
-const app = express();
+import cors from 'cors';
 
+const app = express();
+app.use(cors());
+
+let pokedex = JSON.parse(fs.readFileSync('../data/pokedex.json', 'utf8'));
+
+// Retourn la liste des pokemons et leurs infos
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send(pokedex)
+});
+
+app.get('/pokemon/:id', function (req, res) {
+    const id = req.params.id;
+    const pokemon = pokedex.find(p => p.id == id);
+    res.send(pokemon);
 });
 
 app.listen(3000, function () {
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './style/App.css';
+import './style/pokeType.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokedexData, setPokedexData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
+  const [isEnglish, setIsEnglish] = useState(true);
+  const handleSwitchClick = () => {
+    setIsEnglish(!isEnglish);
+  };
+
+  useEffect(() => {
+    axios.get('http://localhost:3000')
+      .then(res => {
+        setPokedexData(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const filteredData = pokedexData.filter(pokemon =>
+    (isEnglish ? pokemon.name.english.toLowerCase().includes(searchTerm.toLowerCase()) : false) || pokemon.name.french.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      <div className='headerPage'>
+        <h1>Pokedex</h1>
+        <input
+          type="text"
+          placeholder="🇬🇧  & 🇫🇷 🔎"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        <button onClick={handleSwitchClick}>
+          {isEnglish ? '🇬🇧 ➡ 🇫🇷' : '🇫🇷 ➡ 🇬🇧'}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="card-container">
+        {filteredData.map(pokemon => (
+          <div className="card" key={pokemon.id}>
+            {/*
+            <div className='cardContainer-img'>
+              <img className="card-img-top" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt={`Image of ${pokemon.name.english}`} />
+            </div>
+            */}
+            <div className="card-body">
+              <h5 className="card-title">N°{pokemon.id} - {isEnglish ? pokemon.name.english : pokemon.name.french}</h5>
+              <h5>🇯🇵 {pokemon.name.japanese} | 🇨🇳 : {pokemon.name.chinese}</h5>
+              <li className="card-subtitle">Type:
+                {pokemon.type.map(type => (
+                  <span className={`typeShape ${type.toLowerCase()}`} key={type}> {type} </span>
+                ))}
+              </li>
+              <ul className="card-text">
+                <li>HP: {pokemon.base.HP}</li>
+                <li>Attack: {pokemon.base.Attack}</li>
+                <li>Defense: {pokemon.base.Defense}</li>
+                <li>Sp. Attack: {pokemon.base['Sp. Attack']}</li>
+                <li>Sp. Defense: {pokemon.base['Sp. Defense']}</li>
+                <li>Speed: {pokemon.base.Speed}</li>
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-
-export default App
+export default App;
